c945f41ecbcd6890d1ad547e2af597fd279c1190
AntonVoronezh <pstut@mail.ru>
2018-12-13 20:13:30 +0300

    add gulp.task

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
 const debug = require('gulp-debug');
 const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify');
+// const uglify = require('gulp-uglify');
+const uglifyEs = require('gulp-uglify-es').default;
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 const newer = require('gulp-newer');
@@ -24,6 +25,9 @@ const cleanCss = require('gulp-clean-css');
 const uncss = require('gulp-uncss');
 const autoprefixer = require('gulp-autoprefixer');
 const gcmq = require('gulp-group-css-media-queries');
+const rename = require('gulp-rename');
+const svgstore = require('gulp-svgstore');
+const svgmin = require('gulp-svgmin');
 
 
 
@@ -54,12 +58,14 @@ gulp.task('scss', () => {
         .pipe(gulpIf(!isDevelopment, debug({ title: 'autoprefixer -> production' })))
         .pipe(concat('bundle.css'))
         .pipe(debug({ title: 'concat' }))
-        .pipe(gulpIf(!isDevelopment, uncss({ html: ['dist/*.html'], ignore: ['placeholder'] })))
+        .pipe(gulp.dest('dist/css')) 
+        // .pipe(gulpIf(!isDevelopment, uncss({ html: ['dist/*.html'], ignore: ['placeholder'] })))
         .pipe(gulpIf(!isDevelopment, debug({ title: 'uncss -> production' })))
         .pipe(gulpIf(!isDevelopment, cleanCss()))
         .pipe(gulpIf(!isDevelopment, debug({ title: 'cleanCss -> production' })))
         .pipe(gulpIf(isDevelopment, sourcemaps.write()))
         .pipe(debug({ title: 'sourcemaps' }))
+        .pipe(rename('bundle.min.css'))
         .pipe(gulp.dest('dist/css'))
         .pipe(debug({ title: 'dest' }))
         .pipe(gulpIf(isDevelopment, notify('SCSS OK!')))
@@ -93,13 +99,18 @@ gulp.task('js:plus', () => {
 
 gulp.task('js:polyfil', () => {
     return gulp.src('src/js/**/*.js')
+    .pipe(plumber({ errorHandler: onError }))
         .pipe(concat('all.js'))
+        // .pipe(babel())
         .pipe(babel())
-        .pipe(autopolyfiller('polyfill.js'))
-        .pipe(gulpIf(isDevelopment, notify('js:polyfil OK!')))
-        .pipe(gulpIf(!isDevelopment, notify('js:polyfil --- production --- OK!')))
+        // .pipe(uglify())
+        // .pipe(autopolyfiller('all.js'))
+        // .pipe(gulpIf(isDevelopment, notify('js:polyfil OK!')))
+        // .pipe(gulpIf(!isDevelopment, notify('js:polyfil --- production --- OK!')))
         .pipe(gulp.dest('dist/js'));
 });
+
+
 
 gulp.task('js:bundle', () => {
     return gulp.src('src/js/**/*.js')
@@ -107,11 +118,15 @@ gulp.task('js:bundle', () => {
         .pipe(debug({ title: 'js:bundle - plumber' }))
         .pipe(gulpIf(isDevelopment, sourcemaps.init()))
         .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('dist/js'))///////
+        .pipe(gulpIf(!isDevelopment, uglifyEs()))
         .pipe(debug({ title: 'js:bundle - concat' }))
         .pipe(gulpIf(isDevelopment, notify('js:bundle OK!')))
         .pipe(gulpIf(!isDevelopment, notify('js:bundle --- production --- OK!')))
         .pipe(gulpIf(isDevelopment, sourcemaps.write()))
-        .pipe(gulp.dest('dist/js'));
+        .pipe(rename('bundle.min.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream());
 });
 
 
@@ -131,11 +146,14 @@ gulp.task('watch:js', () => {
 // ********************************
 
 gulp.task('imagemin', () => {
-    return gulp.src('src/img/**/*.{png,jpg,jpeg,gif,svg}')
+    return gulp.src('src/img/**/*.{png,jpg,jpeg,gif}')
         .pipe(plumber({ errorHandler: onError }))
         .pipe(newer('dist/img'))
         .pipe(debug({ title: 'IMG - newer' }))
-        .pipe(imagemin())
+        .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 3}),
+            imagemin.jpegtran({progressive: true})
+        ]))
         .pipe(debug({ title: 'IMG - imagemin' }))
         .pipe(gulp.dest('dist/img'))
         .pipe(debug({ title: 'IMG - dest' }))
@@ -147,6 +165,18 @@ gulp.task('watch:imagemin', () => {
     gulp.watch('src/img/**', ['imagemin'])
 });
 
+// ********************************
+
+gulp.task('svg', () => {
+    return gulp.src('src/img/svg/*.svg')
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(svgmin())
+        .pipe(svgstore({inlineSvg: true}))
+        .pipe(rename('sprite.svg'))
+        .pipe(gulp.dest('dist/img/icons'))
+        .pipe(debug({ title: 'SVG - dest' }))
+        .pipe(gulpIf(isDevelopment, notify('SVG OK!')))
+});
 
 // ********************************
 gulp.task('htmlmin', () => {
@@ -160,8 +190,9 @@ gulp.task('htmlmin', () => {
                 removeComments: true,
                 collapseWhitespace: true,
                 preserveLineBreaks: true,
-                // removeEmptyElements: true,
-                // removeEmptyAttributes: true
+                removeEmptyElements: true,
+                removeEmptyAttributes: true,
+                removeRedundantAttributes: true
             }
         )))
         .pipe(debug({ title: 'htmlmin' }))
@@ -186,7 +217,41 @@ gulp.task('serve', () => {
     });
 });
 
+// ********************************
+// gulp.task('build', () => {
+//    run('htmlmin', 'imagemin')
+// });
 
 
 // подключение задач в дефолтные
 gulp.task('default', ['watch:htmlmin', 'watch:scss', 'watch:js', 'watch:imagemin', 'serve']);
+
+
+
+
+// var cssMin = function() {
+//     return gulp.src(paths.css)
+//                 .pipe(cssmin())
+//                 .pipe(rename({suffix: '.min'}))
+//                 .pipe(gulp.dest(srcPath + '/css'));
+// };
+
+// gulp.task('sass-compile', function() {
+//     gulp.src(paths.sass)
+//         .pipe(sass())
+//         .pipe(gulp.dest(srcPath + '/css'))
+//         .on('end', function(){
+//             cssMin();
+//         });
+// });
+
+// тут присутствует метод .on('end', fn). Простыми словами, когда сработает событие end 
+// (создастся css файл) вызываем функцию cssMin(), которая создаст минифицированный css файл.
+
+
+// ./ - текущая директория;
+// ../ - родительская директория;
+// js/index.js - файл index.js в папке js;
+// js/*.js - все файлы с расширением js в папке js;
+// js/**/*.js - все файлы с расширением js в папке js и в ее подкаталогах;
+// !js/*.js - исключение файлов с расширением js в папке js.
---
+++ src/js/1.js
@@ -0,0 +1,10 @@
+const getHead = document.head;
+	        console.log(getHead);
+	
+	        const getBody = document.body;
+	        console.log(getBody.lastChild.previousElementSibling);
+	
+	        console.log(getBody.lastChild.previousElementSibling.lastElementChild);
+
+
+            sfd
